2d0e4762408dbd3b8573e9d89878276e9f61260f
saidam90 <saida.murtazaliyeva@yahoo.com>
2024-07-02 23:35:39 +0400

    Fix GA config and replace cv

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,11 @@ import "@/styles/globals.css";
 import { Poppins } from "next/font/google";
 import { useEffect } from "react";
 import { initGA, logPageView } from "/analytics";
+import ReactGA from "react-ga4";
 
 const poppins = Poppins({ weight: ["100", "200", "400", "700", "900"] });
+
+ReactGA.initialize("G-2940GH5W9Y");
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,20 @@ export default function home() {
         <meta name="description" content="Bleep boop" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-2940GH5W9Y"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2940GH5W9Y');
+          `,
+          }}
+        />
       </Head>
       <Home />
     </>
