e447a4c2d1aa019b1eb5028fcccde65e6b093db9
saidam90 <saida.murtazaliyeva@yahoo.com>
2024-07-02 23:44:39 +0400

    Add GA link in document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,33 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Google Tag Manager script */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-2940GH5W9Y"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-2940GH5W9Y');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
